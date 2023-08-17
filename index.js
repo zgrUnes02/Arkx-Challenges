ed617c8eb32c816e88bc8405fba6e2d00cc18a9c
youness zagouri <younesszagouri2002@gmail.com>
2023-08-17 22:08:17 +0100

    commit the challenge of Http That allow us to looking for a city and write its temperature in a file with its name

--- index.js
+++ index.js
@@ -1,26 +1,42 @@
 
-const fs = require('fs') ;
+const PORT = 4000 || 4001 ;
+const http = require('http') ;
+const url = require('url') ;
 
-const getCityName = ( fileName , temperature ) => {
+const server = http.createServer((req , res) => {
+
+    const weatherApi = [
+        {name : 'casablanca' , temperature : 25} ,
+        {name : 'rabat' , temperature : 34} ,
+        {name : 'settat' , temperature : 44} ,
+        {name : 'titouan' , temperature : 24} ,
+    ] ;
+
+    const myUrl = new URL(`http://localhost:4000${req.url}`) ;
+    const cityName = myUrl.search.slice(myUrl.search.indexOf('=') + 1) ;
+
     try {
-        fs.readFile(fileName , 'utf-8' , (err , content) => {
-            if (err) {
-                console.log(err) ;
-            }
-            else {
-                const textWriteInsideMyNewFile = `The Temperature of ${content} Is ${temperature}*C` ;
-                fs.writeFile(`${content}.txt` , textWriteInsideMyNewFile , (err) => {
-                    if (err) {
-                        console.log(err) ;
-                    }
-                    console.log('The File Has Been Created With Success !') ;
-                })
-            }
-        }) ;
+        const cityNeed = weatherApi.filter(city => city.name == cityName) ;
+        if ( cityNeed.length != 0 ) {
+            const fs = require('fs') ;
+            fs.writeFile(`${cityName}.txt` , `The temperature of ${cityName} is ${cityNeed[0].temperature}` , (err) => {
+                if ( err ) {
+                    console.log('Something went wrong : ' , err) ;
+                }
+                else {
+                    console.log('The Has Been Created With Success !') ; 
+                }
+            }) ;
+        }
     }
-    catch (error) {
+    catch ( error ) {
         console.log(error) ;
     }
-}
+    
+    
+    
+}) ;
 
-getCityName('input.txt' , 34) ;
+server.listen(PORT , () => {
+    console.log(`The server is running on localhost:${PORT}`) ;
+}) ;
